0e68879d64b68bb7b8b582cba8f3422bb6e31a16
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-05-16 21:32:22 +0200

    Add getWeather utility

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+const getWeather = (location: string) => {
+  // choose a random temperature and condition
+  const randomTemperature = Math.floor(Math.random() * 15) + 10;
+  const randomConditionIndex = Math.floor(Math.random() * 5);
+  const conditions = ["Cloudy", "Sunny", "Rainy", "Snowy", "Windy"];
+
+  return {
+    location: location,
+    temperature: randomTemperature,
+    unit: "C",
+    conditions: conditions[randomConditionIndex],
+  };
+};
+
+export { getWeather };
