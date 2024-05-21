3f179aaec2987bd9f12a635380596076dbd7953b
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-05-21 18:05:16 +0200

    Update chat component

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,5 +1,5 @@
 import { useAssistant, Message } from "ai/react";
-import { useRef, useEffect, Dispatch, SetStateAction } from "react";
+import { useRef, useEffect, Dispatch, SetStateAction, useState } from "react";
 import ChatMessage from "./chat-message";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -10,21 +10,43 @@ export default function Chat({
 }: {
   setWeatherData: Dispatch<SetStateAction<Omit<WeatherWidgetProps, "isEmpty">>>;
 }) {
+  const [loading, setLoading] = useState(false);
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const { status, messages, submitMessage, input, handleInputChange } =
     useAssistant({
       api: "/api/assistant",
     });
+
+  useEffect(() => {
+    if (status === "in_progress") {
+      setLoading(true);
+    }
+
+    if (status === "awaiting_message" && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [status]);
 
   const lastDataMessage = messages
     .filter((m: Message) => m.role === "data")
     .pop();
 
   useEffect(() => {
-    setWeatherData((prevWeatherData: Omit<WeatherWidgetProps, "isEmpty">) => ({
-      ...prevWeatherData,
-      ...(lastDataMessage?.data as Object),
-    }));
+    if (lastDataMessage) {
+      setWeatherData(
+        (prevWeatherData: Omit<WeatherWidgetProps, "isEmpty">) => ({
+          ...prevWeatherData,
+          ...(lastDataMessage?.data as Object),
+        })
+      );
+    }
+
+    if ([...messages].pop()?.role === "assistant") {
+      setLoading(false);
+    }
+
+    scrollToBottom();
   }, [messages]);
 
   const scrollToBottom = () => {
@@ -33,20 +55,27 @@ export default function Chat({
     }
   };
 
-  useEffect(() => {
-    scrollToBottom();
-  }, [messages]);
-
   return (
     <div className="flex flex-col-reverse h-full w-full">
       <div className="flex flex-col order-2 flex-grow overflow-y-auto p-3 pt-5 whitespace-pre-wrap space-y-4">
+        {
+          <ChatMessage
+            key="key"
+            role="assistant"
+            content="Hello! How can I assist you today?"
+          />
+        }
         {messages.map((msg, index) => (
           <ChatMessage key={index} role={msg.role} content={msg.content} />
         ))}
+        {loading && (
+          <div className="w-full rounded-3xl animate-pulse bg-gray-300 h-10" />
+        )}
         <div ref={messagesEndRef} />
       </div>
       <form onSubmit={submitMessage} className="flex w-full p-3 pb-10 order-1">
         <Input
+          ref={inputRef}
           type="text"
           className="flex-grow mr-3 bg-[#efefef] py-4 px-6 rounded-3xl border-2 border-transparent border-solid text-base"
           value={input}
